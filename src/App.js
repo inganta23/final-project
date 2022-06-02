648bef643245127efd882f519fa76aba183f100e
inganta23 <johanes230500@gmail.com>
2022-06-02 17:00:26 +0700

    Fixing File Structure

--- src/App.js
+++ src/App.js
@@ -1,9 +1,9 @@
 import { Routes, Route } from "react-router-dom";
-import Login from "./components/Login";
-import Daftar from "./components/Daftar"
+import Login from "./pages/Login";
+import Daftar from "./pages/Daftar"
 import Navbar from "./components/Navbar";
-import Tiket from "./components/Tiket";
-import Beranda from "./components/Beranda";
+import Tiket from "./pages/Tiket";
+import Beranda from "./pages/Beranda";
 import { PayTicket } from "./pages/payTicket";
 
 

d07fea1837878bcbd76103c55bb21fda02b23758
inganta23 <johanes230500@gmail.com>
2022-06-02 16:49:52 +0700

    Fixing Conflict

--- src/App.js
+++ src/App.js
@@ -1,26 +1,19 @@
-sprint-1/johanes/login
 import { Routes, Route } from "react-router-dom";
 import Login from "./components/Login";
 import Navbar from "./components/Navbar";
-
-
 import { PayTicket } from "./pages/payTicket";
-main
 
 function App() {
   return (
     <div className="App">
- sprint-1/johanes/login
-      <Navbar />
-      
+      <Navbar /> 
       <Routes>
-        <Route path="login" element={<Login />}/>
-        {/* <Route path="daftar" element={<Daftar />}/> */}
-      </Routes>
+         <Route path="login" element={<Login />}/>
+      </Routes> 
       
 
-      <PayTicket/>
-main
+      {/* <PayTicket/> */}
+
     </div>
   );
 }
